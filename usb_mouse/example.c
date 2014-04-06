eb3ea432659096ca5e014affdf87105eb78436cb
Ripul Jain <ripulrjain@gmail.com>
2014-04-06 07:55:24 -0400

    Added clicking logic based on waveforms
    Code is configured for printing rather than moving.

--- usb_mouse/example.c
+++ usb_mouse/example.c
@@ -24,9 +24,9 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
-#include "usb_mouse.h"
+//#include "usb_mouse.h"
 
-//#include "usb_serial.h"
+#include "usb_serial.h"
 #include "sampling.h"
 
 #define LED_CONFIG	(DDRD |= (1<<6))
@@ -36,14 +36,12 @@
 #define CPU_16MHz	0x00
 #define CPU_125kHz	0x07
 #define HEX(n) (((n) < 10) ? ((n) + '0') : ((n) + 'A' - 10))
-#define ZERO_G_X	300
-#define ZERO_G_Y	475
+#define MAX_LOW 90
+#define MAX_HIGH 250
 
 #define ACC_ERROR_SENSITIVITY 7
-#define MAX_ACC_CHANGE 85
-#define MULT_FACTOR_OF_DELTA 1
-
-int8_t circle[];
+#define MAX_ACC_CHANGE 95
+#define MULT_FACTOR_OF_DELTA 2
 
 signed long valX;
 signed long valY;
@@ -62,6 +60,17 @@ signed long positionZ[2];
 signed long deltaX;
 signed long deltaY;
 
+unsigned int click_left;
+unsigned int click_right;
+
+unsigned long max;
+
+unsigned short  left_click_pressed;
+unsigned short  left_click_released;
+
+unsigned short right_click_released;
+unsigned short right_click_pressed;
+int flag;
 
 void movement_end_check(void)   
 {
@@ -84,11 +93,13 @@ void movement_end_check(void)
    } 
 }
 
-// Get an average 1024 values of x and y axis
+// Get an average 2048 values of x and y axis
 void Calibrate(void) 
 {
   unsigned int count1;
   count1 = 0;
+  
+  // for calibration of zero g values from accelerometer
   do{  
 	  adc_start(ADC_MUX_PIN_F3, ADC_REF_POWER); //Read X // Y
 	  valX = valX + adc_read();  
@@ -100,25 +111,25 @@ void Calibrate(void)
   valX=valX>>11;                       // division between 2048
   valY=valY>>11;
   
-	_delay_ms(100);
-  count1 = 0;
-  do{  	  	  
-	  adc_start(ADC_MUX_PIN_F6, ADC_REF_POWER);
-	  clickDefault_left = clickDefault_left + adc_read();
+	//_delay_ms(100);
+  //count1 = 0;
+  // do{  	  	  
+	  // adc_start(ADC_MUX_PIN_F6, ADC_REF_POWER);
+	  // clickDefault_left = clickDefault_left + adc_read();
 
-	  adc_start(ADC_MUX_PIN_F7, ADC_REF_POWER);
-	  clickDefault_right = clickDefault_right + adc_read(); 
-	  count1++;
-  }while(count1!=0x0400);                    // 1024 times
+	  // adc_start(ADC_MUX_PIN_F7, ADC_REF_POWER);
+	  // clickDefault_right = clickDefault_right + adc_read(); 
+	  // count1++;
+  // }while(count1!=0x080);                    // 128 times
   
-  clickDefault_left = clickDefault_left >> 11;
-  clickDefault_right = clickDefault_right >> 11;
+  // clickDefault_left = clickDefault_left >> 11;
+  // clickDefault_right = clickDefault_right >> 11;
   
   accelerationx[0] = valX;
   accelerationy[0] = valY;
   
-  left_click[0] = clickDefault_left;
-  right_click[0] = clickDefault_right;
+  // left_click[0] = clickDefault_left;
+  // right_click[0] = clickDefault_right;
 }
 
 void print(signed long value){
@@ -138,7 +149,7 @@ void print(signed long value){
 		buf[i] = mod + '0';
 		i--;
 	}
-	//usb_serial_write((unsigned char *)buf, 5);	
+	usb_serial_write((unsigned char *)buf, 5);	
 }
 
 void position(void)  
@@ -147,88 +158,39 @@ void position(void)
 	char buf[4];
 	char buf1[4];
 	count2=0;
-    
+	
+	// Set right click to 0 everytime, unless we detect a 1
+	click_right = 0;	
+		
+	// get x and y
     do{
 		adc_start(ADC_MUX_PIN_F3, ADC_REF_POWER); //Read X  
 		accelerationx[1]=accelerationx[1] + adc_read(); //filtering routine for noise attenuation
 		
 		adc_start(ADC_MUX_PIN_F5, ADC_REF_POWER); //Read Y
-		accelerationy[1]=accelerationy[1] + adc_read(); //128 samples are averaged. The resulting average represents the acceleration of an instant
+		accelerationy[1]=accelerationy[1] + adc_read(); //64 samples are averaged. The resulting average represents the acceleration of an instant
 			
 		count2++;                                       
-    }while (count2!=0x080);                        // 128 sums of the acceleration sample  
-    accelerationx[1]= accelerationx[1]>>7;          // division by 128
-    accelerationy[1]= accelerationy[1]>>7;
+    }while (count2!=0x040);                        // 64 sums of the acceleration sample  
+    accelerationx[1]= accelerationx[1]>>6;          // division by 64
+    accelerationy[1]= accelerationy[1]>>6;
 	
 	_delay_ms(10);
 	
 	count2=0;
+	
+	// get left and right click
     do{
-		adc_start(ADC_MUX_PIN_F6, ADC_REF_POWER);
+		adc_start(ADC_MUX_PIN_F7, ADC_REF_POWER);
 		left_click[1] = left_click[1] + adc_read();
 		
-		adc_start(ADC_MUX_PIN_F7, ADC_REF_POWER); // 128 sums of the clicks sample  
+		adc_start(ADC_MUX_PIN_F6, ADC_REF_POWER); // 64 sums of the clicks sample  
 		right_click[1] = right_click[1] + adc_read();
 		count2++;                                       
     }while (count2!=0x080); 
-	
-	
-	left_click[1] = left_click[1] >> 7;
-	right_click[1] = right_click[1] >> 7;
-	
-	// if((accelerationy[1] != valY) && (accelerationy[0] != valY)){
-		// if(((accelerationy[1] < valY) && (accelerationy[0] - accelerationy[1] >= 1.5*accelerationy[0]))
-		// || ((accelerationy[1] > valY) && (accelerationy[0] - accelerationy[1] <= 1.5*accelerationy[0]))
-		// )
-		// {
-			// accelerationy[1] = accelerationy[0];
-		// }
-	// }
-		// if(((accelerationy[1] < valY) && (accelerationy[0] - accelerationy[1] >= 1.5*accelerationy[0]))
-		// || ((accelerationy[1] > valY) && (accelerationy[0] - accelerationy[1] <= 1.5*accelerationy[0]))
-		// )
-		// {
-			// accelerationy[1] = accelerationy[0];
-		// }
 		
-	
-	
-	// int k;
-	// for(k=0;k<64;k++){
-		// print(adc[k]);
-		// usb_serial_putchar(' ');
-		// print(adcy[k]);
-		// usb_serial_putchar('\n');
-		// _delay_ms(15);
-	// }
-  
-    // accelerationx[1] = accelerationx[1] - (int)valX; //eliminating zero reference offset of the acceleration data
-    // accelerationy[1] = accelerationy[1] - (int)valY; // to obtain positive and negative acceleration
-  
-    // if ((accelerationx[1] <=3)&&(accelerationx[1] >= -3)) //Discrimination window applied to the X axis acceleration
-      // {accelerationx[1] = 0;}                       
-     
-    // if ((accelerationy[1] <=3)&&(accelerationy[1] >= -3)) 
-      // {accelerationy[1] = 0;} 
-  /*
-	  //first X integration:
-	velocityx[1]= velocityx[0]+ accelerationx[0]+ ((accelerationx[1] -accelerationx[0])>>1);    
-	  //second X integration:
-	positionX[1]= positionX[0] + velocityx[0] + ((velocityx[1] - velocityx[0])>>1);
-	  //first Y integration:       
-	velocityy[1] = velocityy[0] + accelerationy[0] + ((accelerationy[1] -accelerationy[0])>>1);
-	  //second Y integration:       
-	positionY[1] = positionY[0] + velocityy[0] + ((velocityy[1] - velocityy[0])>>1);  
-  
-    accelerationx[0] = accelerationx[1];  //The current acceleration value must be sent to the previous acceleration 
-    accelerationy[0] = accelerationy[1];     //variable in order to introduce the new acceleration value.
-    
-    velocityx[0] = velocityx[1];          //Same done for the velocity variable
-    velocityy[0] = velocityy[1];
-  */
-  
-  
-    //first X integration:
+	left_click[1] = left_click[1] >> 8; // ideally needs to be 7, but 8 works better
+	right_click[1] = right_click[1] >> 8;
 	
 	if (accelerationx[1] <= valX + ACC_ERROR_SENSITIVITY
 		&& accelerationx[1] >= valX - ACC_ERROR_SENSITIVITY){
@@ -246,7 +208,6 @@ void position(void)
 			accelerationx[1] = valX - MAX_ACC_CHANGE ;
 		}
 	
-	
 		//velocityx[1] = velocityx[0] + (accelerationx[1] - accelerationx[0]);
 		velocityx[1] = (accelerationx[1] - valX);
 		positionX[1] = positionX[0] + velocityx[1];
@@ -260,18 +221,7 @@ void position(void)
 		if(deltaX < -127){
 			deltaX = -127;
 		}
-		
-		 // if(((velocityx[1] - velocityx[0]) <= 2) && ((velocityx[1] - velocityx[0]) >= -2)){
-		// //if(abs(velocityx[1] - velocityx[0]) <= 2){
-			// positionX[1] = positionX[0];
-			// deltaX = 0;
-		// }		
-		// else{
-			// positionX[1] = positionX[0] + velocityx[1];
-			// // deltaX = positionX[1] - positionX[0];
-			// deltaX = velocityx[1];
-			// deltaX = deltaX * MULT_FACTOR_OF_DELTA;
-		// }	
+			
 	}
 
 	if (accelerationy[1] <= valY + ACC_ERROR_SENSITIVITY
@@ -317,7 +267,7 @@ void position(void)
 		// }
 	}
 	
-	
+		
     accelerationx[0] = accelerationx[1];  //The current acceleration value must be sent to the previous acceleration 
     accelerationy[0] = accelerationy[1];     //variable in order to introduce the new acceleration value.
     velocityx[0] = velocityx[1];          //Same done for the velocity variable
@@ -325,111 +275,182 @@ void position(void)
 	positionX[0] = positionX[1];          //actual position data must be sent to the  
     positionY[0] = positionY[1];      //previous position
 	
+	/*  |       Press Event          _  <-- Release Event
+	    |        |                 /   \
+		|        v               /       \  
+		|        _             /           \
+		|      /   \         /              \
+		|    /       \ _ _ /                 \
+		|  /  slope 1             slope 2     \
+		|/________________________________________________
+		
+		First If statement updates the max till the press event is reached.
+		Press Event is detected when the current left click value starts becoming less than Max.
+		Once Press Event is reached, we check for max value to be between 90 and 125. If it is, we update click_left to 1
+		
+		Then we wait for the Release event. Release Event is defined as current left click value becoming less than Max for the second time
+		Once Release Event is reached, we release the click button by resetting click_left to 0.
+	*/	
+	if(left_click[1] > max){
+		max = left_click[1];
+		flag = 1;
+	}
+	else{
+		if((max >= MAX_LOW) && (click_left == 0)){
+			click_left = 1;
+			usb_serial_putchar('L');
+			usb_serial_putchar('e');
+			usb_serial_putchar('f');
+			usb_serial_putchar('t');
+			usb_serial_putchar(' ');
+			usb_serial_putchar('P');
+			usb_serial_putchar('r');
+			usb_serial_putchar('e');
+			usb_serial_putchar('s');
+			usb_serial_putchar('s');
+			usb_serial_putchar('!');
+			usb_serial_putchar('\n');
+			_delay_ms(40);
+			flag = 0;
+		}
+		if((max > MAX_LOW) && (click_left == 1) && (flag == 1)){
+			click_left = 0;
+			usb_serial_putchar('L');
+			usb_serial_putchar('e');
+			usb_serial_putchar('f');
+			usb_serial_putchar('t');
+			usb_serial_putchar(' ');
+			usb_serial_putchar('R');
+			usb_serial_putchar('e');
+			usb_serial_putchar('l');
+			usb_serial_putchar('e');
+			usb_serial_putchar('a');
+			usb_serial_putchar('s');
+			usb_serial_putchar('e');
+			usb_serial_putchar('\n');
+		}
+	}
+	
+
 	left_click[0] = left_click[1];
 	right_click[0] = right_click[1];
    
-  
-    // positionX[1] = positionX[1]<<18;      //The idea behind this shifting (multiplication) is a sensibility adjustment.
-    // positionY[1] = positionY[1]<<18;      //Some applications require adjustments to a particular situation i.e. mouse application
-    // data_transfer();  
-    // positionX[1] = positionX[1]>>18;      //once the variables are sent them must return to 
-    // positionY[1] = positionY[1]>>18;      //their original state
+    //To make it work!
+	if(right_click[1] < 120){
+		if((right_click[1] > 60) && (right_click[1] < 90)){
+			click_right = 1;
+			usb_serial_putchar('R');
+			usb_serial_putchar('i');
+			usb_serial_putchar('g');
+			usb_serial_putchar('h');
+			usb_serial_putchar('t');
+			usb_serial_putchar(' ');
+			usb_serial_putchar('B');
+			usb_serial_putchar('i');
+			usb_serial_putchar('t');
+			usb_serial_putchar('c');
+			usb_serial_putchar('h');
+			usb_serial_putchar('e');
+			usb_serial_putchar('s');
+			usb_serial_putchar('!');
+			usb_serial_putchar('\n');
+		}					
+	}
+    	
+	usb_serial_putchar('G');
+	usb_serial_putchar('x');
+	usb_serial_putchar(' ');
+	print(valX);
+	usb_serial_putchar(' ');
+
+	usb_serial_putchar(' ');
+	usb_serial_putchar('G');
+	usb_serial_putchar('y');
+	usb_serial_putchar(' ');
+	print(valY);		
+	usb_serial_putchar(' ');
+
+	usb_serial_putchar(' ');
+
+	usb_serial_putchar(' ');
+	usb_serial_putchar('A');
+	usb_serial_putchar('x');
+	usb_serial_putchar(' ');
+	print(accelerationx[1]);
+	usb_serial_putchar(' ');
+
+	usb_serial_putchar(' ');
+	usb_serial_putchar('A');
+	usb_serial_putchar('y');
+	usb_serial_putchar(' ');
+	print(accelerationy[1]);
+	usb_serial_putchar(' ');
+
+	usb_serial_putchar(' ');
+
+	usb_serial_putchar(' ');
+	usb_serial_putchar('V');
+	usb_serial_putchar('x');
+	usb_serial_putchar(' ');
+	print(velocityx[1]);
+	usb_serial_putchar(' ');
+
+	usb_serial_putchar(' ');
+	usb_serial_putchar('V');
+	usb_serial_putchar('y');
+	usb_serial_putchar(' ');
+	print(velocityy[1]);
+	usb_serial_putchar(' ');
+
+	usb_serial_putchar(' ');
+
+
+	usb_serial_putchar(' ');
+	usb_serial_putchar('X');
+	usb_serial_putchar(' ');
+	print(positionX[1]);
+	usb_serial_putchar(' ');
+
+	usb_serial_putchar(' ');
+	usb_serial_putchar('Y');
+	usb_serial_putchar(' ');
+	print(positionY[1]);
+	usb_serial_putchar(' ');
+
+	usb_serial_putchar(' ');
+
+	usb_serial_putchar(' ');
+	usb_serial_putchar('D');
+	usb_serial_putchar('x');
+	usb_serial_putchar(' ');
+	print(deltaX);
+	usb_serial_putchar(' ');
 	
+	usb_serial_putchar(' ');
+	usb_serial_putchar('D');
+	usb_serial_putchar('y');
+	usb_serial_putchar(' ');
+	print(deltaY);
+	usb_serial_putchar(' ');
 	
-	// usb_serial_putchar('G');
-	// usb_serial_putchar('x');
-	// usb_serial_putchar(' ');
-	// print(valX);
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	// usb_serial_putchar('G');
-	// usb_serial_putchar('y');
-	// usb_serial_putchar(' ');
-	// print(valY);		
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	// usb_serial_putchar('A');
-	// usb_serial_putchar('x');
-	// usb_serial_putchar(' ');
-	// print(accelerationx[1]);
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	// usb_serial_putchar('A');
-	// usb_serial_putchar('y');
-	// usb_serial_putchar(' ');
-	// print(accelerationy[1]);
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	// usb_serial_putchar('V');
-	// usb_serial_putchar('x');
-	// usb_serial_putchar(' ');
-	// print(velocityx[1]);
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	// usb_serial_putchar('V');
-	// usb_serial_putchar('y');
-	// usb_serial_putchar(' ');
-	// print(velocityy[1]);
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	
-	
-	// usb_serial_putchar(' ');
-	// usb_serial_putchar('X');
-	// usb_serial_putchar(' ');
-	// print(positionX[1]);
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	// usb_serial_putchar('Y');
-	// usb_serial_putchar(' ');
-	// print(positionY[1]);
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	// usb_serial_putchar('D');
-	// usb_serial_putchar('x');
-	// usb_serial_putchar(' ');
-	// print(deltaX);
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	// usb_serial_putchar('D');
-	// usb_serial_putchar('y');
-	// usb_serial_putchar(' ');
-	// print(-deltaY);
-	// usb_serial_putchar(' ');
-	
-	// usb_serial_putchar(' ');
-	// usb_serial_putchar('l');
-	// usb_serial_putchar('c');
-	// usb_serial_putchar(' ');
-	// print(left_click[1]);
-	// usb_serial_putchar(' ');
+	usb_serial_putchar(' ');
+	usb_serial_putchar('l');
+	usb_serial_putchar('c');
+	usb_serial_putchar(' ');
+	print(left_click[1]);
+	usb_serial_putchar(' ');
 		
-	// usb_serial_putchar(' ');
-	// usb_serial_putchar('r');
-	// usb_serial_putchar('c');
-	// usb_serial_putchar(' ');
-	// print(right_click[1]);
-	// usb_serial_putchar(' ');	
+	usb_serial_putchar(' ');
+	usb_serial_putchar('r');
+	usb_serial_putchar('c');
+	usb_serial_putchar(' ');
+	print(right_click[1]);
+	usb_serial_putchar(' ');	
 		
-	//usb_serial_putchar('\n');
+	usb_serial_putchar('\n');
 	
-	usb_mouse_move(deltaX,-deltaY, 0); // Move the mouse
-	// usb_mouse_move(1,0, 0); // Move the mouse
+	// usb_mouse_move(deltaY,deltaX,0); // Move the mouse- changed X to Y, Y to X to accomodate change in pins
+	// usb_mouse_buttons(click_left,0,click_right);
 //	_delay_ms(10);
  
 //  movement_end_check();
@@ -437,30 +458,20 @@ void position(void)
     positionX[0] = positionX[1];          //actual position data must be sent to the  
     positionY[0] = positionY[1];      //previous position
    
-    direction = 0;                        // data variable to direction variable reset 
 }    
 
 int main(void)
-{
-	int16_t val_x;
-	int16_t val_y;
-	uint16_t val_left;
-	uint16_t val_right;
+{	
+	click_left = 0;
+	flag = 0;
+	max = 0;
 	
-	int16_t prev_x;
-	
-	prev_x = ZERO_G_X;
-	
-	char buf[4];
-
-
 	CPU_PRESCALE(CPU_125kHz);
 	_delay_ms(1);		// allow slow power supply startup
 	CPU_PRESCALE(CPU_16MHz); // set for 16 MHz clock
 	
 	LED_CONFIG;
 	LED_OFF;
-
 	// Initialize the USB, and then wait for the host to set configuration.
 	// If the Teensy is powered without a PC connected to the USB port,
 	// this will wait forever.
@@ -470,10 +481,7 @@ int main(void)
 	// Wait an extra second for the PC's operating system to load drivers
 	// and do whatever it does to actually be ready for input
 	_delay_ms(1000);
-	
-	valX = 0;
-	valY = 0;
-	
+		
 	accelerationx[0] = 0;
 	accelerationx[1] = 0;
 	accelerationy[0] = 0;
@@ -490,68 +498,23 @@ int main(void)
 	right_click[1] = 0;
 	
 	
+	click_left = 0;
+	click_right = 0;
+
+	max = 0;
+
+	left_click_pressed = 0;
+	left_click_released = 0;
+
+	right_click_released = 0;
+	right_click_pressed = 0;
+		
 	Calibrate();
 
 	while (1) {
 	
 		position();
 		_delay_ms(10);		
-		/* Teensy mouse move code
-		_delay_ms(1000);
-		LED_ON;  // turn the LED on while moving the mouse
-		
-		p = circle;
-		for (i=0; i<36; i++) {
-			x = pgm_read_byte(p++);
-			y = pgm_read_byte(p++);
-			usb_mouse_move(x, y, 0);
-			_delay_ms(20);
-		}
-		LED_OFF;
-		_delay_ms(9000);
-		// This sequence creates a right click
-		//usb_mouse_buttons(0, 0, 1);
-		//_delay_ms(10);
-		//usb_mouse_buttons(0, 0, 0);*/
 	}
 }
 
-int8_t PROGMEM circle[] = {
-16, -1,
-15, -4,
-16, -1,
-14, -7,
-13, -9,
-11, -11,
-9, -13,
-7, -14,
-4, -15,
-1, -16,
--1, -16,
--4, -15,
--7, -14,
--9, -13,
--11, -11,
--13, -9,
--14, -7,
--15, -4,
--16, -1,
--16, 1,
--15, 4,
--14, 7,
--13, 9,
--11, 11,
--9, 13,
--7, 14,
--4, 15,
--1, 16,
-1, 16,
-4, 15,
-7, 14,
-9, 13,
-11, 11,
-13, 9,
-14, 7,
-15, 4,
-16, 1
-};
